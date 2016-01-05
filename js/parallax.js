af0c2303dd6576e41f143cff1af7630e7a4bf5ac
Agnieszka Semrau <agnieszka.semrau@wp.pl>
2016-01-05 13:39:47 +0100

    Merge branch 'master' of https://github.com/infoshareacademy/jfdd-transport
    Conflicts:
    	index.html
    
    FDT-36-animation final

--- js/parallax.js
+++ js/parallax.js
@@ -1,10 +1,5 @@
-/**
- * Created by ASUS on 2016-01-04.
- */
 
 $(document).ready(function(){
-
-
     $(window).on('scroll', function() {
         $('.intro').css('margin-bottom', $(window).scrollTop() * -.7);
     });
