49b72028d83d04409204f7bc09673b8bf1674f65
Agnieszka Semrau <agnieszka.semrau@wp.pl>
2016-01-05 17:09:21 +0100

    Merge branch 'master' of https://github.com/infoshareacademy/jfdd-transport
                          Conflicts:
                          	index.html
    
                          FDT-39-smooth scrolling

--- js/parallax.js
+++ js/parallax.js
@@ -1,12 +1,14 @@
 $(document).ready(function () {
     $(window).on('scroll', function () {
-        $('.intro').css('margin-bottom', $(window).scrollTop() * -.7);
+        $('.intro').css('background-position-y', $(window).scrollTop() * -.7);
     });
 
-    $('a').click(function(){
-        $('html, body').animate({
-            scrollTop: $( $(this).attr('href') ).offset().top
-        }, 500);
+    $('a[href^=#]:not([href=#])').on('click', function () {
+        var element = $($(this).attr('href'));
+        console.log(element.offset().top);
+        $('html,body').animate({ scrollTop: element.offset().top },'normal', 'swing');
         return false;
     });
 });
+
+
