d6ec78ec5dde9e3e2090bdd37fecfeb5d78f95a2
Agnieszka Semrau <agnieszka.semrau@wp.pl>
2016-01-05 09:17:41 +0100

    Merge branch 'master' of https://github.com/infoshareacademy/jfdd-transport
    Conflicts:
    	index.html
    
    FDT-36-animation code review

--- js/parallax.js
+++ js/parallax.js
@@ -3,19 +3,9 @@
  */
 
 $(document).ready(function(){
-    //function parallax(){
-    //    var scrolled = $(window).scrollTop();
-    //    $('.intro').css('top', -(scrolled * 0.1) + 'px');
-    //}
-    //
-    //$(window).scroll(function(){
-    //    parallax();
-    //});
-    //$(window).scroll(function () {
-    //    $(".intro").css("background-position","50% " + ($(this).scrollTop() / 5) + "px");
-    //});
+
 
     $(window).on('scroll', function() {
-        $('.intro').css('margin-top', $(window).scrollTop() * -.10);
+        $('.intro').css('margin-bottom', $(window).scrollTop() * -.7);
     });
 });
