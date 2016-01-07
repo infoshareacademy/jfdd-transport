5787264c5839259df647d55b002a9a0ab352d90e
agaSemrau <agnieszka.semrau@wp.pl>
2016-01-07 21:32:15 +0100

    more-info button, black footer                            finishing changes - nav bar, more info button                            finishing after parallax removal                            animation rename
                               feature/easteregg2 on-click

--- js/easteregg2.js
+++ js/easteregg2.js
@@ -1,30 +1,10 @@
 
-$(function () {
-    //EVENT HANDLERS to detect the key combination which opens the easter egg ('f' + '4'):
+$(document).ready(function () {
     var $easterEgg2 = $('#easterEgg2');
-
-    var pressedKeys = {
-        f: false,
-        4: false
-    };
-
-    $(window).on('keydown', function (e) {
-        if (e.which === 70) {
-            pressedKeys.f = true;
-        }
-        if (e.which === 52 || e.which === 100) { //The key code for '4' on the main keyboard is \
-            // '52' and on the numpad - '100'.
-            pressedKeys[4] = true;
-        }
+    $('#button-gray').on('click', function () {
+        $easterEgg2.removeClass('hide');
     });
 
-    $(window).on('keyup', function (e) {
-        if (pressedKeys.f === true && pressedKeys[4] === true) {
-            $easterEgg2.removeClass('hide');
-        }
-        pressedKeys.f = false;
-        pressedKeys[4] = false;
-    });
 });
 
 
