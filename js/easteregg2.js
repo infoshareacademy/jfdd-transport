50dccab9360201c8e3b4c1e5f4c1b176cf553ef7
agaSemrau <agnieszka.semrau@wp.pl>
2016-01-07 21:35:40 +0100

    feature/easteregg2 - close on click

--- js/easteregg2.js
+++ js/easteregg2.js
@@ -5,6 +5,16 @@ $(document).ready(function () {
         $easterEgg2.removeClass('hide');
     });
 
+    $('#closeEasterEgg2').on('click', function () {
+        $easterEgg2.addClass('hide');
+    });
+
+    $('#startEasterEggGame2').on('click', function () {
+        game.hideGameIntro();
+        playGame();
+    });
+
+
 });
 
 
