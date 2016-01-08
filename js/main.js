60dafa67596d9413f6622cc013f955bf9a9dfb7b
wprzebieracz <przebieracz@gmail.com>
2016-01-08 09:21:13 +0100

    Fade in when scroll is 100px from top of object

--- js/main.js
+++ js/main.js
@@ -45,11 +45,11 @@ $(document).ready(function () {
         /* Check the location of each desired element */
         $('#functionsSection img, #form-div ').each(function (i) {
 
-            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var _100pxOfObject = $(this).offset().top + 100;
             var bottom_of_window = $(window).scrollTop() + $(window).height();
 
             /* If the object is completely visible in the window, fade it it */
-            if (bottom_of_window > bottom_of_object) {
+            if (bottom_of_window > _100pxOfObject) {
                 $(this).removeClass('hide').addClass('animated transparent fadeInUpLite');
             }
         });
