d98826e21cf38e5b2b4ce19420029cea8e6e8b0e
agaSemrau <agnieszka.semrau@wp.pl>
2016-01-09 14:13:41 +0100

    feature/easteregg2 -proby canvas

--- js/easteregg2.js
+++ js/easteregg2.js
@@ -1,28 +1,58 @@
 
 $(document).ready(function () {
-    var $easterEgg2 = $('#easterEgg2');
+//
+//    //var $easterEgg2 = $('#easterEgg2');
     $('#button-gray').on('click', function () {
-        $easterEgg2.removeClass('hide');
+        startGame();
     });
 
-    $('#closeEasterEgg2').on('click', function () {
-        $easterEgg2.addClass('hide');
-    });
+    var myGamePiece;
+    function startGame() {
+        myGameArea.start();
+        $("#myCanvas").removeClass("hide");
+        myGamePiece = new component(30, 30, "red", 10, 120);
+    }
 
-    $('#startEasterEggGame2').on('click', function () {
-        playGame();
-    });
+    var myGameArea = {
+        canvas: document.createElement("canvas"),
+        start: function () {
+            this.canvas.width = 480;
+            this.canvas.height = 270;
+            this.context = this.canvas.getContext("2d");
+            document.getElementById('myCanvas');
+        }
+    }
 
-    var playGame = function () {
-        $('#easterEgg2Intro').addClass('hide');
-        $('#easterEgg2').addClass('gameBackground');
-        $('#gameScore').removeClass('hide');
+    function component(width, height, color, x, y) {
+        this.width = width;
+        this.height = height;
+        this.x = x;
+        this.y = y;
+        ctx = myGameArea.context;
+        ctx.fillStyle = color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+});
 
-    };
-
-
-
-            });
+//
+//    $('#closeEasterEgg2').on('click', function () {
+//        $easterEgg2.addClass('hide');
+//    });
+//
+//    $('#startEasterEggGame2').on('click', function () {
+//        playGame();
+//    });
+//
+//    var playGame = function () {
+//        $('#easterEgg2Intro').addClass('hide');
+//        $('#easterEgg2').addClass('gameBackground');
+//        $('#gameScore').removeClass('hide');
+//
+//    };
+//
+//
+////
+//            });
 
 
 
