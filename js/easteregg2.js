294815ffea571c069b444ff8716a899260cac2d8
agaSemrau <agnieszka.semrau@wp.pl>
2016-01-10 12:10:43 +0100

    feature/easteregg2 -canvas buses still not moving

--- js/easteregg2.js
+++ js/easteregg2.js
@@ -39,23 +39,23 @@ $(document).ready(function () {
         //this.type = type;
         this.width = width;
         this.height = height;
-        this.angle = 0;
-        this.speed = 1;
+        //this.angle = 0;
+        //this.speed = 1;
         this.x = x;
         this.y = y;
         this.update = function() {
             ctx = myGameArea.context;
-            ctx.save();
-            ctx.translate(this.x, this.y);
-            ctx.rotate(this.angle);
+            //ctx.save();
+            //ctx.translate(this.x, this.y);
+            //ctx.rotate(this.angle);
             ctx.fillStyle = color;
-            ctx.fillRect(this.width / -2, this.height / -2, this.width, this.height);
-            ctx.restore();
+            ctx.fillRect(this.x, this.y, this.width, this.height);
+            //ctx.restore();
         }
-        this.newPos = function() {
-            this.x += this.speed * Math.sin(this.angle);
-            this.y -= this.speed * Math.cos(this.angle);
-        }
+        //this.newPos = function() {
+        //    this.x += this.speed * Math.sin(this.angle);
+        //    this.y -= this.speed * Math.cos(this.angle);
+        //}
     }
 
 function component2(width, height, color, x, y) {
