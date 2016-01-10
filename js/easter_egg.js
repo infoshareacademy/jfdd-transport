99c7328000cafde488fb549e1101c898a749c880
agmo <mor.agnieszka@gmail.com>
2016-01-10 21:31:43 +0100

    Refactor code

--- js/easter_egg.js
+++ js/easter_egg.js
@@ -1,6 +1,7 @@
 $(function () {
     //EVENT HANDLERS to detect the key combination which opens the easter egg ('f' + '4'):
     var $easterEgg = $('#easterEgg');
+    var $startGameBtn = $('#startEasterEggGame');
     var gameover = false;
     var result = 0;
     var gameTimeouts = [];
@@ -28,7 +29,7 @@ $(function () {
             $easterEgg.removeClass('donotdisplay');
 
             //EVENT HANDLER for the start game button:
-            $('#startEasterEggGame').on('click', function () {
+            $startGameBtn.on('click', function () {
                 game.hideGameIntro();
                 playGame();
             });
@@ -52,7 +53,7 @@ $(function () {
             game.resetGame();
             $('#easterEggIntro').removeClass('donotdisplay');
         } else {
-            $('#startEasterEggGame').off();
+            $startGameBtn.off();
             $easterEgg.addClass('donotdisplay');
         }
     });
@@ -60,14 +61,14 @@ $(function () {
     //GAMEPLAY
     var playGame = function () {
         gameStarted = true;
-        $('#startEasterEggGame').off();
+        $startGameBtn.off();
         gameover = false;
         gameTimeouts = [];
         game.startGameCountdown(game.time);
         game.generateBuses(3, [1, 2, 3]);
 
-        $('.vehicles').each(function (index, element) {
-            game.runABus(element, generateRandomValue(0, 1500), 350); //From 0 up to 1500 milliseconds \
+        $('.vehicles').each(function () {
+            game.runABus(this, generateRandomValue(0, 1500), 350); //From 0 up to 1500 milliseconds \
             // so as not to have the user waiting too long for the buses to show up at the start \
             // of the game.
         });
@@ -94,13 +95,11 @@ $(function () {
                 if (seconds < 1) {
                     clearInterval(game.counter.id[0]);
                     game.counter.isRunning = false;
-                    console.log('Is counter running: ' + game.counter.isRunning);
                     endGame();
                 }
             };
 
             game.counter.id[0] = setInterval(decrementCountdown, 1000);
-            console.log('Counter id is: ' + game.counter.id);
         },
         counter: {
             isRunning: false,
@@ -111,13 +110,13 @@ $(function () {
                 var busDestination = $(e.target).parent().attr('data-destination'); //The 'data-destination' \
                 // attribute holds information about the bus destination. The div where the bus sits is a \
                 // parent of the door div.
+
                 if (busDestination === 'obc') {
                     if (e.type !== 'dblclick') { //A double click is effectively counted as three clicks: \
                         // two regular clicks plus one double click (which would give the user 3 points instead \
-                        // of 2 even though they clicked twice). Points are therefore given only for the single \
+                        // of 2, even though they clicked twice). Points are therefore given only for the single \
                         // clicks that were registered as part of a double click.
                         result += 1;
-                        console.log('result ' + result + ', event type: ' + e.type); //TODO Remove after testing.
                     }
                 } else {
                     if (e.type !== 'dblclick') {
@@ -125,9 +124,10 @@ $(function () {
                     }
                     result = Math.max(0, result); //The counter doesn't go below zero, \
                     // so the user never gets a negative value score. If they lose all the points \
-                    // by clicking the wrong buses, the counter will get reset to zero (the max method will \
+                    // by clicking the wrong buses, the counter gets reset to zero (the max method will \
                     // return zero for a negative value) and counting will start all over again.
                 }
+
                 $('#currentScore').text(result);
             });
         },
@@ -140,8 +140,9 @@ $(function () {
             }
 
             var drivingSpeed = generateRandomValue(400, 1200);
+
             //Record timeout ID to the gameTimeouts array so that the timeout can be\
-            //be stopped at the end of the game. (Same for all the timeouts).
+            //be stopped at the end of the game. (Same for all the timeouts and the interval).
             gameTimeouts[0] = window.setTimeout(function () {
                 $(whichBus).animate({left: whereToGo + 'px'}, drivingSpeed, function () {
                     if ($(whichBus).hasClass('inService')) {
@@ -196,18 +197,15 @@ $(function () {
                 return;
             }
 
-            console.log(howManyToGenerate + "; " + indexes);
             for (var i = 0; ; i++) {
-                //var busColors = [];
                 var $bus = $('<div>');
                 var $doorLeft = $('<div>');
                 var $doorRight = $('<div>');
                 var busColor = game.busColors[generateRandomValue(0, game.busColors.length - 1)];
 
                 $bus
-                    .addClass('inService vehicles vehicle' + indexes[i])
+                    .addClass('inService vehicles vehicle' + indexes[i]) //A bus that's 'inService' can pick up passengers.
                     .css('background-color', busColor);
-                //A bus that's 'inService' can pick up passengers.
                 $doorLeft
                     .addClass('vehicleDoor vehicleDoorLeft')
                     .appendTo($bus);
@@ -309,7 +307,6 @@ $(function () {
         clearGameTimeouts: function () {
             for (var i = 0; i < gameTimeouts.length; i++) {
                 clearTimeout(gameTimeouts[i]);
-                console.log('clearing timeout ' + gameTimeouts[i]);
             }
         },
         showResults: function () {
@@ -321,7 +318,6 @@ $(function () {
             $('#gameResults').addClass('hide');
             $('#endOfGameMask').addClass('hide')/*.fadeOut()*/;
             $('.vehicles').remove(); //Remove existing buses.
-
             $('.infoBoards').text(''); //Clear destinations from infoboards.
             $('#currentScore').text('0'); //Clear the displayed result.
             result = 0; //Reset the variable holding current result.
@@ -329,8 +325,6 @@ $(function () {
         stopGame: function () {
             $easterEgg.off(); //Turns off the handler for counting clicks.
             gameover = true;
-
-            console.log(gameTimeouts);
             game.clearGameTimeouts();
         },
         playAgain: function () {
