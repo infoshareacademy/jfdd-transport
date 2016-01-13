42592fc3aa0d5627462c49df6d4c1d1e8433f8d0
Agnieszka Semrau <agnieszka.semrau@wp.pl>
2016-01-13 16:20:02 +0100

    points count

--- js/easteregg2.js
+++ js/easteregg2.js
@@ -1,27 +1,28 @@
-var linie = [41, 47, 14,];
+var linie = [41, 47, 14];
 var points = [];
+
 
 $(document).ready(function () {
 
-    var $easterEgg2 = $('#easterEgg2');
-    $('#button-gray').on('click', function () {
-        $('#easterEgg2').removeClass('hide');
-    });
+    var iniciateGame = function () {
+            $('#easterEgg2').removeClass('hide');
+            $('#closeEasterEgg2 span').removeClass('hide');
+            $('#easterEgg2Intro').removeClass('hide');
+    };
 
-    $('#closeEasterEgg2').on('click', function () {
-        $easterEgg2.addClass('hide');
-    });
+    var closeGame = function () {
+        $('#easterEgg2').addClass('hide');
+    };
 
     var playGame = function () {
         $('#easterEgg2Intro').addClass('hide');
-        $easterEgg2.removeClass('hide');
-        $easterEgg2.addClass('gameBackground');
+        $('#easterEgg2').removeClass('hide').addClass('gameBackground');
         $('#gameScore').removeClass('hide');
+        $('#prawaAutorskie a').removeClass('hide');
 
-        grass = document.createElement('div');
-        $(grass).addClass('grass')
-            .appendTo($("#easterEgg2"));
-
+        Array.prototype.random = function () {
+            return this[Math.floor((Math.random()*this.length))];
+        };
 
         var sec = 20;
         var running = true;
@@ -30,97 +31,106 @@ $(document).ready(function () {
             if (sec == -1) {
                 $('#gameScore').fadeOut('fast');
                 clearInterval(timer);
+                closeGame();
+                alert(
+                    "Koniec gry! \nTwój wynik: " +
+                    points.reduce(function(aktualnyWynik,kolejnyPunkt){
+                        return aktualnyWynik+kolejnyPunkt;
+                    }, 0)
+                );
             }
         }, 1000);
-
     };
 
+
+
     var busOnGo = function(){
+        var refreshIntervalId = setInterval(busOnGo, 2000);
+
         setInterval(function() {
 
             var b1 = document.createElement('div');
                     $(b1).addClass('bus1 buses')
                         .html(linie.random())
                         .appendTo($("#easterEgg2"))
-                        .animate({"left": "1500px"}, generateRandomValue(2500, 6500))
+                        .animate({"left": "1800px"}, RandomValue(2000, 6500))
                         .css('left', function () {
                             return $(this).offset().left;
                         })
                         .click(function () {
                             $(this).remove();
 
-                            if (linie.random()== 41) {
-                                $(this).click(points.push(1))
-                            } else {points.push(-10)}
-
-
+                            if ($(this).html()== 41) {
+                              points.push(1);
+                            } else {
+                                points.push(-10);
+                            }
                         });
 
             b2 = document.createElement('div');
             $(b2).addClass('bus2 buses')
                 .html(linie.random())
                 .appendTo($("#easterEgg2"))
-                .animate({"left":"1500px"}, generateRandomValue(2500, 6500))
                 .css('left', function(){ return $(this).offset().left; })
-                .animate({"left":"1500px"}, generateRandomValue(2500, 6500))
+                .animate({"left":"1800px"}, RandomValue(2000, 6500))
                 .click(function () {
                     var actualPosition = $(b2).offset().left;
                     $(this).remove();
-                })
+                });
 
             b3 = document.createElement('div');
             $(b3).addClass('bus3 buses')
                 .html(linie.random())
                 .appendTo($("#easterEgg2"))
-                .animate({"right":"1500px"}, generateRandomValue(2500, 6500))
                 .css('right', function(){ return $(this).offset().right; })
-                .animate({"right":"1500px"}, generateRandomValue(2500, 6500))
+                .animate({"right":"1800px"}, RandomValue(2000, 6500))
                 .click(function () {
                     var actualPosition = $(b3).offset().left;
                     $(this).remove();
-                })
+                    if ($(this).html()== 41) {
+                        points.push(1);
+                    } else {
+                        points.push(-10);
+                    }
+                });
 
             b4 = document.createElement('div');
             $(b4).addClass('bus4 buses')
                 .html(linie.random())
                 .appendTo($("#easterEgg2"))
                 .css('right', function(){ return $(this).offset().right; })
-                .animate({"right":"1500px"}, generateRandomValue(2500, 6500))
+                .animate({"right":"1800px"}, RandomValue(2000, 6500))
                 .click(function () {
                     var actualPosition = $(b4).offset().left;
                     $(this).remove();
+                    if ($(this).html()== 41) {
+                        points.push(1);
+                    } else {
+                        points.push(-10);
+                    }
                 })
 
 
-        }, 3000);
-    }
-
-
-    Array.prototype.random = function () {
-        return this[Math.floor((Math.random()*this.length))];
+        }, 2500);
+        clearInterval(refreshIntervalId);
     };
-
-    var sumArray = function(points) {
-        for (var index = 0,
-               length = points.length,
-               sum = 0;
-            index < length;
-            sum += points[index++]
-        );
-        return sum;
-    };
-
 
     $('#startEasterEggGame2').on('click', function () {
         playGame();
-        //runBus(6);
         busOnGo();
+    });
+
+    $('.footer_image').on('click', function () {
+        iniciateGame();
+    });
+
+    $('#closeEasterEgg2').on('click', function () {
+        closeGame();
     });
 });
 
-
-//Helper functions from AgMo
-function generateRandomValue(minValToGenerate, maxValToGenerate) {
-    return Math.floor((Math.random() * (maxValToGenerate - minValToGenerate + 1)) + minValToGenerate);
+//Helper function from AgMo
+function RandomValue(min, max) {
+    return Math.floor((Math.random() * (max - min + 1)) + min);
 }
 
