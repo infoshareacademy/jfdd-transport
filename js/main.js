cc1ee2b1f254ce84c8f4efe59688e2aa7941ab5a
annokr <annokr1@gmail.com>
2016-01-15 00:16:30 +0100

    FTD-40: podstrony z opisami funkcjonalnośc

--- js/main.js
+++ js/main.js
@@ -58,14 +58,14 @@ $(document).ready(function () {
     /**
      * Handling cookies to display information about cookies
      */
-     $('footer').prepend(
+    $('footer').prepend(
         $('<div/>', {'class': 'cookieInfo'}).append(
             $('<p/>', {'class': 'cookieInfoTitle', text: 'Ciastka!'})
             )
             .append(
                 $('<p/>', {'class': 'cookieInfoText', text: 'Strona korzysta z cookies w celu realizacji usług i zgodnie z Polityką Cookies. Możesz określić warunki używania cookies w Twojej przeglądarce. Kliknij, aby zamknąć.'})
-                )
-            );
+            )
+    );
 
 
 
