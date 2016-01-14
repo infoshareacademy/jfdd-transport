1430b6c16a46bdb859b35d3efe21535b16952dfa
wprzebieracz <przebieracz@gmail.com>
2016-01-14 11:39:10 +0100

    fix background problem, was related to speed of scroll

--- js/wp_game.js
+++ js/wp_game.js
@@ -1,49 +1,43 @@
-// a piece representing player
 var myGamePiece;
 var myObstacle;
-var myScore;
+//var myScore;
 var myBackground;
 
-// function startung game, and creating new player
 function startGame() {
     myGameArea.start();
     myGamePiece = new component(100, 50, "images/Untitled.png", 10, 120, "image");
-    myScore = new component("30px", "Consolas", "black", 280, 40, "text");
-    myObstacle = new component(10, 200, "green", 300, 120);
-    myBackground = new component(656, 270, "images/threelane.png", 0, 0, "image");
+    //myScore = new component("30px", "Consolas", "black", 280, 40, "text");
+    myObstacle = new component(100, 50, "images/Untitled.png", 50, 200 , "obstacle");
+    myBackground = new component(456, 270, "images/threelane.png", 0, 0, "background");
 }
 
-// Playable canvas
 var myGameArea = {
     canvas: document.createElement("canvas"),
     start: function () {
-        this.canvas.width = 480;
+        this.canvas.width = 465;
         this.canvas.height = 270;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-        // set interval on gameArea
         this.interval = setInterval(updateGameArea, 20);
         window.addEventListener('keydown', function (e) {
             myGameArea.key = e.keyCode;
-        })
+        });
         window.addEventListener('keyup', function (e) {
             myGameArea.key = false;
         })
     },
     clear: function () {
-        // clear everything
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     },
 
     stop: function () {
         clearInterval(this.interval);
     }
-}
+};
 
-// constructor to create new player
 function component(width, height, color, x, y, type) {
     this.type = type;
-    if (type == "image") {
+    if (type == "image" || type == "background" || type == "obstacle") {
         this.image = new Image();
         this.image.src = color;
     }
@@ -54,29 +48,34 @@ function component(width, height, color, x, y, type) {
     this.speedX = 0;
     this.speedY = 0;
     this.update = function () {
-        //passing canvas here
         ctx = myGameArea.context;
         if (this.type == "text") {
             ctx.font = this.width + " " + this.height;
             ctx.fillStyle = color;
             ctx.fillText(this.text, this.x, this.y);
-        } else if (type == "image") {
+        } else if (type == "image" || type == "background" || type == "obstacle") {
             ctx.drawImage(this.image,
                 this.x,
                 this.y,
                 this.width, this.height);
+            if (type == "background") {
+                ctx.drawImage(this.image,
+                    this.x + this.width, this.y, this.width, this.height);
+            }
         } else {
-            // setting fill style of draw
             ctx.fillStyle = color;
-            //draw sth on canvas
             ctx.fillRect(this.x, this.y, this.width, this.height);
         }
-        //ctx.drawImage(img, x, y, 50, 30);
-    }
+    };
     this.newPos = function () {
         this.x += this.speedX;
         this.y += this.speedY;
-    }
+        if (this.type == "background") {
+            if (this.x == -(this.width)) {
+                this.x = 0;
+            }
+        }
+    };
     this.crashWith = function (otherobj) {
         var myleft = this.x;
         var myright = this.x + (this.width);
@@ -105,7 +104,12 @@ function updateGameArea() {
         myBackground.speedX = -1;
         myBackground.newPos();
         myBackground.update();
-        myObstacle.x += -1;
+        myObstacle.speedX = -1;
+        myObstacle.newPos();
+        myObstacle.update();
+        //myObstacle.x += -1;
+
+
         myGamePiece.speedX = 0;
         myGamePiece.speedY = 0;
         if (myGameArea.key && myGameArea.key == 65) {
@@ -122,7 +126,6 @@ function updateGameArea() {
         }
         myGamePiece.newPos();
         myGamePiece.update();
-        myObstacle.update();
     }
 }
 
@@ -146,6 +149,3 @@ function stopMove() {
     myGamePiece.speedX = 0;
     myGamePiece.speedY = 0;
 }
-
-var img = new Image;
-img.src = "images/Untitled.png";
