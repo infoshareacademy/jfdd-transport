be09650adb1dfdc6a96d12856bf8e620ec918d9f
anna <annokr1@gmail.com>
2016-01-14 16:43:07 +0100

    FTD-40- change funkcje.js

--- js/funkcje.js
+++ js/funkcje.js
@@ -2,7 +2,7 @@
  * Created by anna on 13.01.16.
  */
 $(document).ready(function() {
-    $('.function1').click(function(){
+    $('.function').click(function(){
         var div = $(this).attr('div');
         $('#opis1').load(div);
         return false
